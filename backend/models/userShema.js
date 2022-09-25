b04a5634365e00fe4dbde20683894247b31c7ec1
AsfourMohammad <mohammad.asfo@gmail.com>
2022-09-25 23:09:42 +0300

    HashPass

--- backend/models/userShema.js
+++ backend/models/userShema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 //creare User Schema
 const userSchema= new mongoose.Schema({
     firstName:{ type:String,required:true},
@@ -11,6 +12,8 @@ const userSchema= new mongoose.Schema({
     password:{ type: String, required: true} ,
     role:{type:mongoose.Schema.Types.ObjectId,ref:"roles"}
 });
-
+userSchema.pre("save",async function(){
+    this.password=await bcrypt.hash(this.password,10)
+})
 
 module.exports=mongoose.model("user",userSchema);
