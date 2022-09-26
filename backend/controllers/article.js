bddd63a732eb8eb185f0e0f254e269e013f97725
AsfourMohammad <mohammad.asfo@gmail.com>
2022-09-26 21:27:05 +0300

    post

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -23,10 +23,23 @@ newArticle
   });
 });
 };
+const updateArticle = (req, res)=>{
+  const _id = req.params.id;
+  articlesModel
+    .findByIdAndUpdate(_id, req.body, { new: true })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({ success: false,message: ` ${_id} not found`});
+      }
+      res.status(202).json({success: true,message: 'Article updated',article: result,});
+    })
+    .catch((err) => {
+      res.status(500).json({success: false,message: 'Server Error', err: err.message,});
+    });
+};
 
 
 
 
 
-
-module.exports={createNewArticle};
+module.exports={createNewArticle,updateArticle};
--- backend/routes/article.js
+++ backend/routes/article.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
 
-
-const {createNewArticle}=require("../controllers/article");
+const {createNewArticle,updateArticle}=require("../controllers/article");
 const { createNewComment } = require("./../controllers/comment");
 
 
 
 
 const articlesRouter = express.Router();
-const authentication = require("../middleware/authentication");
-const authorization = require("../middleware/authorization");
+
 
 articlesRouter.post("/", createNewArticle);
+articlesRouter.put("/:id", updateArticle);
 
 articlesRouter.post("/:id/comments",createNewComment);
 module.exports = articlesRouter;
