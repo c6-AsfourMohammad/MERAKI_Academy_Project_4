87bb1aba2f87c3a203c5782378e62e0352989273
AsfourMohammad <mohammad.asfo@gmail.com>
2022-09-27 15:20:12 +0300

    Frontend

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext, useState,useEffect } from "react";
+import "./style.css";
+import axios from "axios";
 
 const Login = () => {
   return <div>Login</div>;
--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useContext, useState,useEffect } from "react";
+import "./style.css";
+import axios from "axios";
+
+
+
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [bio, setBIo] = useState("");
+  const [imgProfile, setiImgProfile] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role,setRole]=useState("6330b552f198916cada996eb");
   return <div>Register</div>;
 };
 export default Register;
