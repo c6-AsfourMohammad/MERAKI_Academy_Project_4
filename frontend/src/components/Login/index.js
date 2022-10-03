754bc66e4eb3dfcc33ab225ed858bc858e0f7612
AsfourMohammad <mohammad.asfo@gmail.com>
2022-10-03 20:15:55 +0300

    login google

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -4,10 +4,11 @@ import "./style.css";
 import axios from "axios";
 import { newContext } from "../../App";
 
-// import Popup from 'reactjs-popup';
+ import Popup from 'reactjs-popup';
 import {GoogleLogin,GoogleLogout} from "react-google-login"
 import {Link, useNavigate }from 'react-router-dom'
 import { useGoogleLogin } from 'react-google-login'
+import ReactDOM from 'react-dom';
 
 
 const Login = () => {
@@ -78,6 +79,7 @@ const responseGoogle = (response) => {
 // const Failure=(result)=>{
 //   alert(result)
 // }
+
   return <div className="Login"> 
   
     <div className="LoginName">Login</div>
@@ -87,7 +89,9 @@ const responseGoogle = (response) => {
       {/* create login with Google */}
     </div>
     <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    className="Google"
+    clientId="667694760446-9ot0r7lbg6r8senhbff2112monoi00a6.apps.googleusercontent.com"
+    clientSecret="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"
       buttonText="Login With Google"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
@@ -104,11 +108,11 @@ const responseGoogle = (response) => {
         setPassword(e.target.value)
     }} type="password" placeholder="Password"/>
     {/* logButton */}
-      <button className="LoginButton" onClick={loginUser}>Login</button>
+      {/* <button className="LoginButton" onClick={loginUser}>Login</button> */}
      
-      {/* <Popup className="popup" trigger={ <button className="LoginButton" onClick={loginUser}>Login</button>} position="right center">
+       <Popup className="popup" trigger={ <button className="LoginButton" onClick={loginUser}>Login</button>} position="right center">
     <div>welcome again</div>
-  </Popup> */}
+  </Popup> 
  
     </div>;
 };
